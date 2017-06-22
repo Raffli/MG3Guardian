2bf72802910cdcf3002070928b177e451dde2176
Nikolai Stecker <Nikolai.Stecker@gmx.de>
2017-06-22 19:30:53 +0200

    neu gebuildet und sound eingebaut

---
+++ Guardian/Assets/musiScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class musiScript : MonoBehaviour
+{
+	void Start()
+	{
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+		DontDestroyOnLoad (gameObject);
+		audio.Play(44100);
+	}
+}
