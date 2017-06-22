e392ae09f14511815f3ea75f5eb6e2edefb5b477
Nikolai_Laptop <Nikolai.stecker@gmx.de>
2017-06-22 18:25:56 +0200

    viele änderungen und build eingefügt

--- Guardian/Assets/Eva.cs
+++ Guardian/Assets/Eva.cs
@@ -7,12 +7,16 @@ public class Eva : MonoBehaviour {
 	public GameObject cow;
 	public GameObject eva;
 	public GameObject text;
+    public GameObject exitButton;
+    public GameObject rig;
 
 	void OnTriggerEnter (Collider other){
 		if (other.tag == "Adam") {
 			eva.SetActive (false);
 			cow.SetActive (true);
 			text.SetActive (true);
+            exitButton.SetActive(true);
+            rig.SetActive(true);
 		}
 	}
 }
