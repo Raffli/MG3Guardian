eb0bc35690a0eeb6a2d74534723d9550b364fea5
Nikolai Stecker <Nikolai.Stecker@gmx.de>
2017-06-22 17:46:43 +0200

    cave level angepasst, adam und hände haben keine collision mehr, andere level weiter an leap angepasst

---
+++ Guardian/Assets/Scripts/movementCave.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class movementCave : MonoBehaviour {
+
+	Animator anim;
+	public bool walk;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		walk = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (walk == true) {
+
+
+
+			anim.SetBool ("isWalking", true);
+			anim.SetBool ("isIdle", false);
+			transform.Translate(0, 0, speed/100 );
+
+		} else if (walk == false) {
+
+			anim.SetBool ("isWalking", false);
+			anim.SetBool ("isIdle", true);
+
+		}
+			
+		
+		}
+	void OnCollisionEnter(Collision collision)
+	{
+		print ("collision");
+		print (collision.gameObject.tag);
+		if (collision.gameObject.tag == "Player") {
+			print ("player collision-------------------------");
+			Physics.IgnoreCollision (collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.tag == "MoveStopper") {
+			walk = false;
+		} else if (other.tag == "enemy") {
+			walk = false;
+			SceneManager.LoadScene (10);
+		} else if (other.tag == "MoveStarter"){
+			walk = true;
+		} else if (other.tag == "FinishField") {
+			walk = false;
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex+1);
+		} else if (other.tag == "DeadField") {
+			SceneManager.LoadScene (10);
+			walk = false;
+		} else if (other.tag == "Mushroom") {
+			print (transform.rotation.y);
+			transform.Rotate (new Vector3 (0, 180, 0));
+			Collider col = other.gameObject.GetComponent<Collider> ();
+			col.enabled = false;
+			//col.isTrigger = false;
+		} else if (other.tag == "LightHand") {
+			walk = true;
+		}else if (other.tag == "RightMover") {
+			transform.eulerAngles = new Vector3(
+				transform.eulerAngles.x,
+				90,
+				transform.eulerAngles.z
+			);
+		}else if (other.tag == "LeftMover") {
+			transform.eulerAngles = new Vector3(
+				transform.eulerAngles.x,
+				270,
+				transform.eulerAngles.z
+			);
+		}else if (other.tag == "Stalaktit") {
+			walk = false;
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if (other.tag == "LightHand") {
+			walk = false;
+		} else if (other.tag == "Stalaktit") {
+			walk = true;
+		}
+	}
+}
